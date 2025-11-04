b192edfcb9014e550d0e98b4f20d305e9f0c0aac
David Brandt <David@Brandt-IT.consulting>
2025-11-04 02:14:32 +0100

    add  "https://huggingface.co/lllyasviel/Annotators/resolve/main/RealESRGAN_x4plus.pth"

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -78,6 +78,7 @@ VAE_MODELS=(
 )
 
 ESRGAN_MODELS=(
+    "https://huggingface.co/lllyasviel/Annotators/resolve/main/RealESRGAN_x4plus.pth"
     "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth"
     "https://huggingface.co/FacehugmanIII/4x_foolhardy_Remacri/resolve/main/4x_foolhardy_Remacri.pth"
     "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x_NMKD-Siax_200k.pth"
